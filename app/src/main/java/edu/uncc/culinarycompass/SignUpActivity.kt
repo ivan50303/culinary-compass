ae0b58e0d0b86bde9477b9d081379e1c98ddaa2d
Ivan Rojas <irojas1@uncc.edu>
2024-03-15 14:30:35 -0400

    Added sign up page

---
+++ app/src/main/java/edu/uncc/culinarycompass/SignUpActivity.kt
@@ -0,0 +1,21 @@
+package edu.uncc.culinarycompass
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+
+        val signUpBtn = findViewById<Button>(R.id.signupbtn)
+        signUpBtn.setOnClickListener {
+            var intent = Intent(this@SignUpActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
