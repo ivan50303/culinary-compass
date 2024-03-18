88ab6b34b902d6a82b0ab048e738a334cbc2a5f1
jensen <jensenlobo30@outlook.com>
2024-03-18 16:28:45 -0400

    discover carousel completed

--- app/src/main/java/edu/uncc/culinarycompass/HomeActivity.kt
+++ app/src/main/java/edu/uncc/culinarycompass/HomeActivity.kt
@@ -1,21 +1,95 @@
 package edu.uncc.culinarycompass
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.carousel.CarouselSnapHelper
+import edu.uncc.culinarycompass.adapter.CarouselAdapter
 import edu.uncc.culinarycompass.adapter.MainCategoryAdapter
 import edu.uncc.culinarycompass.entities.Recipes
+import edu.uncc.culinarycompass.models.ImageItem
+import java.util.UUID
 
 class HomeActivity : AppCompatActivity() {
     var arrMainCategory = ArrayList<Recipes>()
     var mainCategoryAdapter = MainCategoryAdapter()
+    private lateinit var viewpager2 : ViewPager2
+    private lateinit var pageChangeListener: ViewPager2.OnPageChangeCallback
 
+    private val params = LinearLayout.LayoutParams(
+        LinearLayout.LayoutParams.WRAP_CONTENT,
+        LinearLayout.LayoutParams.WRAP_CONTENT
+    ).apply {
+        setMargins(8,0,8,0)
+    }
 
-
+    private lateinit var carouselRecyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        viewpager2 = findViewById<ViewPager2>(R.id.carouselRecyclerView)
+
+
+        // this is just sample data
+        // we can actually use a full url instead, that will call the API for the image
+        val imageList = arrayListOf(
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "android.resource://" + packageName + "/" + R.drawable.pancakes
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "android.resource://" + packageName + "/" + R.drawable.paella
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "android.resource://" + packageName + "/" + R.drawable.corndogs
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "android.resource://" + packageName + "/" + R.drawable.grilled_cheese
+            )
+        )
+
+        val carouselAdapter = CarouselAdapter()
+        viewpager2.adapter = carouselAdapter
+        carouselAdapter.submitList(imageList)
+
+        val slideDotll = findViewById<LinearLayout>(R.id.slideDots)
+        val dotsImage = Array(imageList.size) { ImageView(this) }
+
+        dotsImage.forEach{
+            it.setImageResource(
+                R.drawable.non_active_dot
+            )
+            slideDotll.addView(it, params)
+        }
+        dotsImage[0].setImageResource(R.drawable.active_dot)
+
+        pageChangeListener = object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                dotsImage.mapIndexed { index, imageView ->
+                   if (position == index){
+                        imageView.setImageResource(
+                            R.drawable.active_dot
+                        )
+                   }else{
+                       imageView.setImageResource(R.drawable.non_active_dot)
+                   }
+                }
+                super.onPageSelected(position)
+            }
+        }
+        viewpager2.registerOnPageChangeCallback(pageChangeListener)
+
+        //setUpCarouselRecyclerView()
+
         val rv_main_category = findViewById<RecyclerView>(R.id.rv_main_category)
         arrMainCategory.add(Recipes(1, "Beef"))
         arrMainCategory.add(Recipes(2, "Pizza"))
@@ -27,5 +101,24 @@ class HomeActivity : AppCompatActivity() {
         rv_main_category.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         rv_main_category.adapter = mainCategoryAdapter
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        viewpager2.unregisterOnPageChangeCallback(pageChangeListener)
+    }
+
+    /*private fun setUpCarouselRecyclerView(){
+        carouselRecyclerView = findViewById(R.id.carouselRecyclerView)
+        CarouselSnapHelper().attachToRecyclerView(carouselRecyclerView)
+        carouselRecyclerView.adapter = CarouselAdapter(images = getImages())
+    }
+
+    private fun getImages(): List<String> {
+        return listOf(
+            "https://a.uguu.se/VuTbYhCE.jpg",
+            "https://a.uguu.se/pftGWNfc.jpg",
+            "https://a.uguu.se/mwbGwrvc.jpg"
+        )
+    }*/
 
 }
---
+++ app/src/main/java/edu/uncc/culinarycompass/adapter/CarouselAdapter.kt
@@ -0,0 +1,48 @@
+package edu.uncc.culinarycompass.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import edu.uncc.culinarycompass.R
+import edu.uncc.culinarycompass.models.ImageItem
+
+class CarouselAdapter : ListAdapter<ImageItem,CarouselAdapter.ViewHolder>(DiffCallback()){
+
+    class DiffCallback : DiffUtil.ItemCallback<ImageItem>(){
+        override fun areItemsTheSame(oldItem: ImageItem, newItem: ImageItem): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: ImageItem, newItem: ImageItem): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+    class ViewHolder(iteView: View): RecyclerView.ViewHolder(iteView){
+        private val imageView = iteView.findViewById<ImageView>(R.id.imageViewCarousel)
+
+        fun bindData(item: ImageItem){
+            Glide.with(itemView)
+                .load(item.url)
+                .into(imageView)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.image_item_layout,parent,false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val imageItem = getItem(position)
+        holder.bindData(imageItem)
+    }
+}
