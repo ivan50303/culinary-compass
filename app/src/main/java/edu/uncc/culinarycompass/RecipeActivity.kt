0f010d439eec2350017ce6b5daf1cec91ed0daa5
Ivan Rojas <irojas1@uncc.edu>
2024-03-21 11:23:34 -0400

    finish recipe page

---
+++ app/src/main/java/edu/uncc/culinarycompass/RecipeActivity.kt
@@ -0,0 +1,13 @@
+package edu.uncc.culinarycompass
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class RecipeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe)
+
+        supportActionBar?.hide()
+    }
+}
