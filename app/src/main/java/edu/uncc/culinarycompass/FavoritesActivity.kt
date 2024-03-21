818d74b3b750ee3736f20b0894eaa3ade05ffc20
Ivan Rojas <irojas1@uncc.edu>
2024-03-21 12:00:38 -0400

    completed favorites page

---
+++ app/src/main/java/edu/uncc/culinarycompass/FavoritesActivity.kt
@@ -0,0 +1,13 @@
+package edu.uncc.culinarycompass
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class FavoritesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorites)
+
+        supportActionBar?.hide()
+    }
+}
